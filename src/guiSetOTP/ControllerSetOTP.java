25f45589f8d5887ea5fea465bb7f572e8caf8332
Edis Jakupovic <edisjak.main@gmail.com>
2026-09-16 10:39:36 -0700

    whoops forgot the actual new files

---
+++ src/guiSetOTP/ControllerSetOTP.java
@@ -0,0 +1,132 @@
+package guiSetOTP;
+
+import database.Database;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
+import javafx.scene.control.ComboBox;
+
+/*******
+ * <p> Title: ControllerSetOTP Class. </p>
+ *
+ * <p> Description: The Java/FX-based Set One-time Password Page. </p>
+ *
+ * @author Edis Jakupovic
+ *
+ * @version 1.00        2025-09-15 Initial version
+ */
+
+public class ControllerSetOTP {
+	/**
+	 * Default constructor is not used.
+	 */
+	public ControllerSetOTP() {}
+
+	private static Database theDatabase = applicationMain.FoundationsMain.database;
+
+	/**********
+	 * <p> Method: doSelectUser() </p>
+	 *
+	 * <p> Description: This method uses the ComboBox widget, fetches which item in the ComboBox
+	 * was selected (a user in this case), and establishes that user and the current user, setting
+	 * easily accessible values without needing to do a query. </p>
+	 *
+	 */
+	protected static void doSelectUser() {
+		ViewSetOTP.theSelectedUser = (String)ViewSetOTP.combobox_SelectUser.getValue();
+		theDatabase.getUserAccountDetails(ViewSetOTP.theSelectedUser);
+		setupSelectedUser();
+	}
+
+    /**********
+     * <p> Method: setupSelectedUser() </p>
+     *
+     * <p> Description: </p>
+     */
+    private static void setupSelectedUser() {
+        if (ViewSetOTP.theSelectedUser.compareTo("<Select a User>") == 0) {
+            ViewSetOTP.label_UserDetails.setText("User: " + ViewSetOTP.theUser.getUserName());
+            return;
+        }
+        ViewSetOTP.text_SetPassword.setText("");
+        ViewSetOTP.label_UserDetails.setText("Setting One-Time Password for: " + ViewSetOTP.theSelectedUser);
+    }
+
+
+	/**********
+	 * <p> Method: repaintTheWindow() </p>
+	 *
+	 * <p> Description: This method determines the current state of the window and then establishes
+	 * the appropriate list of widgets in the Pane to show the proper set of current values. </p>
+	 *
+	 */
+	protected static void repaintTheWindow() {
+		ViewSetOTP.theRootPane.getChildren().clear();
+
+		if (ViewSetOTP.theSelectedUser.compareTo("<Select a User>") == 0) {
+			// Only show the request to select a user to be updated and the ComboBox
+			ViewSetOTP.theRootPane.getChildren().addAll(
+                ViewSetOTP.label_PageTitle, ViewSetOTP.label_UserDetails,
+                ViewSetOTP.button_UpdateThisUser, ViewSetOTP.line_Separator1,
+                ViewSetOTP.label_SelectUser, ViewSetOTP.combobox_SelectUser,
+                ViewSetOTP.line_Separator4, ViewSetOTP.button_Return,
+                ViewSetOTP.button_Logout, ViewSetOTP.button_Quit
+            );
+		}
+		else {
+			// Show all the fields as there is a selected user (as opposed to the prompt)
+			ViewSetOTP.theRootPane.getChildren().addAll(
+                ViewSetOTP.label_PageTitle, ViewSetOTP.label_UserDetails,
+                ViewSetOTP.button_UpdateThisUser, ViewSetOTP.line_Separator1,
+                ViewSetOTP.label_SelectUser,
+                ViewSetOTP.combobox_SelectUser,
+                ViewSetOTP.line_Separator4,
+                ViewSetOTP.label_SetPassword,
+                ViewSetOTP.text_SetPassword,
+                ViewSetOTP.button_RandomPassword,
+                ViewSetOTP.button_Return,
+                ViewSetOTP.button_Logout,
+                ViewSetOTP.button_Quit
+            );
+		}
+
+		ViewSetOTP.theStage.setTitle("CSE 360 Foundation Code: Set One-Time Password Page");
+		ViewSetOTP.theStage.setScene(ViewSetOTP.theSetOTPScene);
+		ViewSetOTP.theStage.show();
+	}
+
+
+	/**********
+	 * <p> Method: performReturn() </p>
+	 *
+	 * <p> Description: This method returns the user (who must be an Admin as only admins are the
+	 * only users who have access to this page) to the Admin Home page. </p>
+	 *
+	 */
+	protected static void performReturn() {
+		guiAdminHome.ViewAdminHome.displayAdminHome(ViewSetOTP.theStage, ViewSetOTP.theUser);
+	}
+
+	/**********
+	 * <p> Method: performLogout() </p>
+	 *
+	 * <p> Description: This method logs out the current user and proceeds to the normal login
+	 * page where existing users can log in or potential new users with a invitation code can
+	 * start the process of setting up an account. </p>
+	 *
+	 */
+	protected static void performLogout() {
+		guiUserLogin.ViewUserLogin.displayUserLogin(ViewSetOTP.theStage);
+	}
+
+	/**********
+	 * <p> Method: performQuit() </p>
+	 *
+	 * <p> Description: This method terminates the execution of the program.  It leaves the
+	 * database in a state where the normal login page will be displayed when the application is
+	 * restarted.</p>
+	 *
+	 */
+	protected static void performQuit() {
+		System.exit(0);
+	}
+}
---
+++ src/guiSetOTP/ModelSetOTP.java
@@ -0,0 +1,15 @@
+package guiSetOTP;
+
+/*******
+ * <p> Title: ModelSetOTP Class. </p>
+ *
+ * <p> Description: The SetOTP Page Model.  This class is not used as there is no
+ * data manipulated by this MVC beyond accepting role information and saving it in the
+ * database.</p>
+ *
+ * @author Edis Jakupovic
+ *
+ * @version 1.00        2025-09-15 Initial version
+ *
+ */
+public class ModelSetOTP {}
