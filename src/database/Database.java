d56be24ece7121864f267419e7703d6a29caac3f
Shane McPhillips <smcphillips99@gmail.com>
2026-09-16 20:11:58 -0400

    Removed .settings/ folder, added correct database file, removed duplicate alertemailerror variable issue

--- src/database/Database.java
+++ src/database/Database.java
@@ -543,34 +543,39 @@ public class Database {
 	    }
 		return;
 	}
-	
+
 	/*******
 	 * <p> Method: void updatePassword(String username, String firstName) </p>
-	 * 
+	 *
 	 * <p> Description: Update the password of a user given that user's username and the new
 	 *		password.</p>
 	 *
-	 *<p> This stores the password in plaintext. Do we need to hash this later?
-	 * 
+	 * <p> This stores the password in plaintext. Do we need to hash this later? </p>
+	 *
 	 * @param username is the username of the user
-	 * 
+	 *
 	 * @param password is the new password for the user
-	 *  
+	 *
 	 */
-	// update the first name
-	public void updatePassword(String username, String password) {
-	    String query = "UPDATE userDB SET password = ? WHERE username = ?";
+	public void updatePassword(String username, String password, boolean oneTimePassword) {
+	    String query = "UPDATE userDB SET password = ?, oneTimePassword = ? WHERE username = ?";
 	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
 	        pstmt.setString(1, password);
-	        pstmt.setString(2, username);
+            pstmt.setBoolean(2, oneTimePassword);
+	        pstmt.setString(3, username);
 	        pstmt.executeUpdate();
 	        currentPassword = password;
+            currentOneTimePassword = oneTimePassword;
+            System.out.println("*** database.updatePassword: " + pstmt.toString());
 	    } catch (SQLException e) {
 	        e.printStackTrace();
 	    }
 	}
-	
-	
+
+	public void updatePassword(String username, String password) {
+        updatePassword(username, password, false);
+	}
+
 	/*******
 	 * <p> Method: String getFirstName(String username) </p>
 	 *
