153e3fec2f38985bd215e2ef492fef247c86752e
Edis Jakupovic <edisjak.main@gmail.com>
2026-09-18 18:17:17 -0700

    fix: change TextLengthEvaluator to return error message

--- src/cse360/GRP/ADES/evaluator/textLengthEvaluation/TextLengthChangeListener.java
+++ src/cse360/GRP/ADES/evaluator/textLengthEvaluation/TextLengthChangeListener.java
@@ -6,36 +6,36 @@ import javafx.scene.control.TextField;
 
 /*******
  * <p> Title: TextLengthChangeListener Class. </p>
- * 
+ *
  * <p> Description: The TextLengthChangeListener class is used as an event handler listener for incoming changes to text-box fields and
  * checks for compliance with correct text length.
  * page. </p>
- * 
+ *
  * <p> Copyright: Shane McPhillips © 2026 </p>
- * 
+ *
  * @author Shane McPhillips
- * 
+ *
  * @version 1.00		2026-09-14 Initial version
- *  
+ *
  */
 
 public class TextLengthChangeListener implements ChangeListener<String> {
-	
+
 	private TextField textField; //TextField that the listener is watching for.
-	
+
 	private boolean shouldClearText = false; //Option to clear text upon character count error.
 	private boolean isTextUpdating = false; //Check for if the text field is being adjusted (asynchronous task prevention)
-	
+
 	public TextLengthChangeListener(TextField textField) {
 		this(textField, false);
 	}
-	
+
 	public TextLengthChangeListener(TextField textField, boolean shouldClearText) {
 		this.textField = textField;
 		this.shouldClearText = shouldClearText;
 	}
 
-	//Needed method for implement class. Controls text-field text length and shows dialogue 
+	//Needed method for implement class. Controls text-field text length and shows dialogue
 	//if text is over allowed length.
 	@Override
 	public void changed(ObservableValue<? extends String> observed, String oldText, String newText) {
@@ -44,10 +44,12 @@ public class TextLengthChangeListener implements ChangeListener<String> {
 			return;
 		}
 		//Compare length to max allowed length.
-		if (newText.length() > TextLengthEvaluator.MAX_TEXT_LENGTH) {
+        String errMessage = TextLengthEvaluator.evaluateText(newText);
+
+		if (errMessage != "") {
 			//Show dialogue box for warning on input length.
-			TextLengthEvaluator.instance().showAlertDialogue();
-			
+			TextLengthEvaluator.instance().showAlertDialogue(errMessage);
+
 			//Must use run-later to disable UI updates from effecting current method.
 			javafx.application.Platform.runLater(() -> {
 				this.isTextUpdating = true; //Set currently updating text
--- src/cse360/GRP/ADES/evaluator/textLengthEvaluation/TextLengthEvaluator.java
+++ src/cse360/GRP/ADES/evaluator/textLengthEvaluation/TextLengthEvaluator.java
@@ -5,34 +5,33 @@ import javafx.scene.control.Alert.AlertType;
 
 /*******
  * <p> Title: TextLengthEvaluator Class. </p>
- * 
- * <p> Description: The TextLengthEvaluator class is used as a singleton class to provide 
+ *
+ * <p> Description: The TextLengthEvaluator class is used as a singleton class to provide
  * the event handler co-listener class a dialogue warning box.
  * This class also provides the home for the MAX_TEXT_LENGTH setting.
  * page. </p>
- * 
+ *
  * <p> Copyright: Shane McPhillips © 2026 </p>
- * 
+ *
  * @author Shane McPhillips
- * 
+ *
  * @version 1.00		2026-09-14 Initial version
- *  
+ *
  */
 
 public class TextLengthEvaluator {
 	private Alert textLengthAlert = new Alert(AlertType.WARNING); //Alert box
 	public static final int MAX_TEXT_LENGTH = 32; //Set max length for all input texts
-	
+
 	private static TextLengthEvaluator instance = null; //Singleton instance
-	
+
 	//Constructor initializes alert box to correct settings.
 	private TextLengthEvaluator() {
 		textLengthAlert.setTitle("Incorrect Input Length!");
-		textLengthAlert.setHeaderText("The input length is over the maximum allowed character count.");
-		textLengthAlert.setContentText("Please correct the input to use a maximum of " 
+		textLengthAlert.setContentText("Please correct the input to use a maximum of "
 		+ MAX_TEXT_LENGTH + " characters!");
 	}
-	
+
 	//Retrieve an instance of the singleton.
 	public static TextLengthEvaluator instance() {
 		if (instance == null) {
@@ -40,9 +39,14 @@ public class TextLengthEvaluator {
 		}
 		return instance;
 	}
-	
+
+    public static String evaluateText(String newText) {
+        return newText.length() > MAX_TEXT_LENGTH ? "The input length is over the maximum allowed character count." : "";
+    }
+
 	//Call on alert box to show and wait for the user to click out of the box.
-	public void showAlertDialogue() {
+	public void showAlertDialogue(String errMessage) {
+        textLengthAlert.setHeaderText(errMessage);
 		this.textLengthAlert.showAndWait();
 	}
 }
